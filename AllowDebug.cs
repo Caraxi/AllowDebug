951c1c3a2979e972c8ece9205ef40f646876af8f
Cara <git@caraxian.com>
2021-01-20 02:03:35 +1030

    Don't remove

--- AllowDebug.cs
+++ AllowDebug.cs
@@ -1,38 +1,22 @@
 ﻿using Dalamud.Plugin;
 using System;
-using System.Diagnostics;
 using System.Runtime.InteropServices;
 
 namespace AllowDebug {
     public class AllowDebug : IDalamudPlugin {
-
-        [DllImport("kernel32.dll", SetLastError = true)]
-        private static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, [Out] byte[] lpBuffer, int dwSize, out IntPtr lpNumberOfBytesRead);
-
-        [DllImport("kernel32.dll", SetLastError = true)]
-        private static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, int nSize, out IntPtr lpNumberOfBytesWritten);
-
         public string Name => "AllowDebug";
-        public DalamudPluginInterface PluginInterface { get; private set; }
-
-        private IntPtr scanAddress;
-        private readonly byte[] nop = new byte[] { 0x31, 0xC0, 0x90, 0x90, 0x90, 0x90 };
-        private byte[] original;
+        private readonly byte[] nop = { 0x31, 0xC0, 0x90, 0x90, 0x90, 0x90 };
 
         public void Initialize(DalamudPluginInterface pluginInterface) {
-            this.PluginInterface = pluginInterface;
-            original = new byte[nop.Length];
-            scanAddress = PluginInterface.TargetModuleScanner.ScanText("FF 15 ?? ?? ?? ?? 85 C0 74 11");
-            if (scanAddress != IntPtr.Zero) {
+            try {
+                var scanAddress = pluginInterface.TargetModuleScanner.ScanText("FF 15 ?? ?? ?? ?? 85 C0 74 11");
+                if (scanAddress == IntPtr.Zero) throw new Exception();
                 PluginLog.Log($"Overwriting Debug Check @ 0x{scanAddress.ToInt64():X}");
-                ReadProcessMemory(Process.GetCurrentProcess().Handle, scanAddress, original, nop.Length, out _);
-                WriteProcessMemory(Process.GetCurrentProcess().Handle, scanAddress, nop, nop.Length, out _);
+                Marshal.Copy(nop, 0, scanAddress, nop.Length);
+            } catch {
+                PluginLog.Log("Failed to overwrite debug check.");
             }
         }
-
-        public void Dispose() {
-            if (scanAddress != IntPtr.Zero && original != null) WriteProcessMemory(Process.GetCurrentProcess().Handle, scanAddress, original, original.Length, out _);
-        }
-
+        public void Dispose() { }
     }
 }
